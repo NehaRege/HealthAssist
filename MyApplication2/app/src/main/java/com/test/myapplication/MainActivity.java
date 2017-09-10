151007019d5c9b531bbab0a20a7d2584cd6690e3
Neha Rege <NehaRege@Nehas-MacBook-Pro.local>
2017-09-10 13:14:19 -0700

    worked on the login xml layout

--- MyApplication2/app/src/main/java/com/test/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,13 +1,39 @@
 package com.test.myapplication;
 
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+
+        final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
+        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
+
+        usernameWrapper.setHint("Username");
+        passwordWrapper.setHint("Password");
+    }
+
+    private void initializeViews() {
+        buttonLogin = (Button) findViewById(R.id.activity_main_login_button);
+    }
+
+
 }
