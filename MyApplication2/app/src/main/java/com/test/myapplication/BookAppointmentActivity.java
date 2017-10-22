2bf9e469b532b029d24527183a4210759a27c5ee
Neha Rege <NehaRege@Nehas-MacBook-Pro.local>
2017-10-22 04:49:03 -0700

    added weight sttribute to margining and added initial setup for booking appointments

---
+++ MyApplication2/app/src/main/java/com/test/myapplication/BookAppointmentActivity.java
@@ -0,0 +1,9 @@
+package com.test.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by NehaRege on 10/22/17.
+ */
+public class BookAppointmentActivity extends AppCompatActivity {
+}
